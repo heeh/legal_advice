9cd7facace7afb4dbfdf18b32cba6b20a1f8e7db
Hee Hwang <heehcs@gmail.com>
2020-04-02 09:06:24 -0400

    Clean up

--- parser.py
+++ parser.py
@@ -1,8 +1,8 @@
 import json
-data = []
-with open('/Users/heeh/Data/legaladvice/2015-01.txt') as f:
-    for line in f:
-        data.append(json.loads(line))
 
-print(len(data))        
-print(data[0]['selftext'])
+with open("pretty_outfile.json", "r") as f:
+    json_object = json.load(f)
+print(len(json_object['data']))
+
+def main():
+    
